7c7ce2302d9a6899f90c0108c4af5e0e7f861331
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2021-11-25 19:32:57 +0000

    Added rock movement

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    public GameObject obj;
+    public Transform start;
+    public Transform end;
+    public bool moving;
+    public bool moved;
+    public float moveSpeed = 0.3f;
+
     void Start()
     {
-        
+        moving = false;
+        moved = false;
+        obj.transform.position = start.position;
+        obj.transform.rotation = start.rotation;
     }
     
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (moving)
+        {
+            obj.transform.position = Vector3.MoveTowards(obj.transform.position, end.position, moveSpeed);
+            obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, end.rotation, moveSpeed);
+
+            
+            if (obj.transform.position == end.position && obj.transform.rotation == end.rotation)
+            {
+                moving = false;
+                moved = true;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E) && !moving && !moved)
+            {
+                moving = true;
+            }
+        }
     }
 }
